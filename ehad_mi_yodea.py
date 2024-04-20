94da7f914fbe45059f021c16b3c814fbc583b505
motyzk <motyzk@gmail.com>
2024-04-20 20:19:17 +0300

    exercise

---
+++ ehad_mi_yodea.py
@@ -0,0 +1,34 @@
+a - [
+    "placeholder1",
+    "placeholder2",
+    "placeholder3",
+    "placeholder4",
+    "placeholder5",
+    "placeholder6",
+    "placeholder7",
+    "placeholder8",
+    "placeholder9",
+    "placeholder10",
+    "placeholder11",
+    "placeholder12",
+    "placeholder13",
+]
+
+
+def b(e):
+    def c(f):
+        print(f, a[f - 1])
+        if f > 1:
+            c(f - 1)
+    print(e, "mi yodea?")
+    print(e, "ani yodea!")
+    c(e)
+    print()
+
+
+def d():
+    for i in range(1, 14):
+        b(i)
+
+
+d()
